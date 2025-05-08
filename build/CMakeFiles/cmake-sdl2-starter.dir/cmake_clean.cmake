5ee3aac67efb27c88ff669f02d7878ff74d1cf4b
Aliff Amir <aliff6406@gmail.com>
2025-05-08 13:21:10 +0100

    use PROJECT_NAME for executable name

---
+++ build/CMakeFiles/cmake-sdl2-starter.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake-sdl2-starter.dir/main.cpp.o"
+  "CMakeFiles/cmake-sdl2-starter.dir/main.cpp.o.d"
+  "cmake-sdl2-starter"
+  "cmake-sdl2-starter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmake-sdl2-starter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/cmake-sdl2-starter.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for cmake-sdl2-starter.
+# This may be replaced when dependencies are built.
